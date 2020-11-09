475d8d93ca338b954448254998f13e3d468998e4
jesseleeruiz <jesseleeruiz@gmail.com>
2020-11-09 11:36:21 -1000

    Created Xcode Project. Added Framework Model. Added image assets. Added mock data.

---
+++ Frameworks/Frameworks/FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameworksApp.swift
+//  Frameworks
+//
+//  Created by Jesse Ruiz on 11/9/20.
+//
+
+import SwiftUI
+
+@main
+struct FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FrameworkGridView()
+        }
+    }
+}
