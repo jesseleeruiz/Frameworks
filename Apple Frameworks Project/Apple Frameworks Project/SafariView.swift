1120f701a3aba110466969f14013e1e08b7d1f17
jesseleeruiz <jesseleeruiz@gmail.com>
2020-11-10 13:07:23 -1000

    Added Safari View so when user taps on Learn More button it takes them to the site where they can find more information about the framework.

---
+++ Apple Frameworks Project/Apple Frameworks Project/SafariView.swift
@@ -0,0 +1,22 @@
+//
+//  SafariView.swift
+//  Apple Frameworks Project
+//
+//  Created by Jesse Ruiz on 11/9/20.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {}
+}
+
+
